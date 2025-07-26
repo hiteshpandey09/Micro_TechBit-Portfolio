7ef63e594f4b4814c5b6f7afc49d0670dd0db190
Hitesh Kumar Pandey <hp00695200@gmail.com>
2025-07-26 20:55:15 +0530

    change1

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: '/Micro_TechBit-Portfolio/',
+ // base: '/Micro_TechBit-Portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
